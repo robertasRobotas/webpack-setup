76b0b8a8cdf4c3e32eab53f625607710338ddd95
Robertas <r.ankudovicius@gmail.com>
2023-09-15 10:24:10 +0300

    Added perfect code

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("var item = \"HAT\";\nconsole.log(item);\nvar fn = function () {\n    console.log(\"another log\");\n};\nfn();\nvar a = 123123;\nvar b = 44444;\nvar c = a + b;\nconsole.log(c);\nconsole.log(\"dddd\");\nconsole.log(\"eeee\");\nvar x;\n\n\n//# sourceURL=webpack://wbepack-setup/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+const item = "HAT";
+
+console.log(item);
+
+const fn = (): void => {
+      console.log("another log");
+};
+
+fn();
+
+const a = 123123;
+
+const b = 44444;
+
+const c = a + b;
+
+console.log(c);
+
+console.log("dddd");
+
+console.log("eeee");
+
+const x = 0;
+
+var yyy = "yyyy";
+
+const userName: any = "Tadas";
+
+console.log(userName);
